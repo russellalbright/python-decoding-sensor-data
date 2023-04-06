2c3ad74aa50f8411bec33c3af1b0db3f22fb5baf
Russell Albright <russell.albright@sas.com>
2023-04-06 13:46:04 -0400

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -20,8 +20,8 @@ house_info=HouseInfo(data)
 test_area=1
 recs =  house_info.get_data_by_area("id",rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
-test_data=datetime.strptime("5/9/20","%m/%d/%y")
-recs = house_info.get_data_by_date("id",rec_date=test_data)
+test_date=datetime.strptime("5/9/20","%m/%d/%y")
+recs = house_info.get_data_by_date("id",rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(
      test_data.strftime("%m/%d/%y"), len(recs)))
 
