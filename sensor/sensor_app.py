2c6129a28e012b127c28513fc843cbc2200c1208
Russell Albright <russell.albright@sas.com>
2023-04-06 13:36:49 -0400

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,6 @@
 from load_data import load_sensor_data
 from house_info import HouseInfo
-from date import datetime
+from datetime import date
 from datetime import datetime
 # Runner script for all modules
 
