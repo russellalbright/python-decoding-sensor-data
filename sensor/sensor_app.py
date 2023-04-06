bcaf5b2330e4e77b9237709f2e12774c69601174
Russell Albright <russell.albright@sas.com>
2023-04-06 11:35:27 -0400

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,4 +1,5 @@
 from load_data import load_sensor_data
+from house_info import HouseInfo
 # Runner script for all modules
 
 
@@ -13,6 +14,11 @@ print("Sensor Data App")
 data= load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
+house_info=HouseInfo(data)
+test_area=1
+recs =  house_info.get_data_by_area("id",rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
+
 
 # Module 3 code here:
 
